55116740ae943dec6052322ae918fe094e8db14c
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-05-11 21:20:53 -0400

    view function works. working on adding different items to tables

--- server.js
+++ server.js
@@ -62,10 +62,7 @@ function view() {
         }
     ])
     .then(function(answer) {
-        const choice=answer.choice;
-        const sqlString="SELECT * FROM " + choice;
-        
-        connection.query(sqlString,{}, function(err,result) {
+        connection.query("SELECT * FROM ",answer.choice, function(err,result) {
             if (err) throw err;
             console.table(result);
             start();
@@ -73,6 +70,7 @@ function view() {
         );
     });
 }
+
 // MYSQL QUERY ADD INFO 
 function addTo() {
     inquirer
@@ -83,49 +81,149 @@ function addTo() {
             message: "Which set records would you like to add to?",
             choices:["Departments","Roles","Employees"]
         }
-    ]).then(function(answer) {
+    ])
+    .then(function(answer) {
         const choice=answer.choice;
-        const sqlString="SELECT * FROM " + choice;
-        connection.query(sqlString, function(err,result){
-            console.log(result);
-        })    
+        switch (choice) {
+            case "Departments":
+            addDepartment();
+            break;
+            
+            case "Roles":
+            addRole();
+            break;
+            
+            case "Employees":
+            addEmployee();
+            break;
+            
+            default:
+            break;
+        }
         
     });
 }    
+
 // MYSQL QUERY UPDATE EMPLOYEE ROLE
 function update() {
     // GET ALL THE EMPLOYEES 
-    connection.query("SELECT first_name, last_name, role_id FROM employees", function(err,result) {
+    connection.query("SELECT first, last, role_id FROM employees", function(err,result) {
         if (err) throw err;
-        console.log(result);
+        resultMapped=result.map();
+        console.log(resultMapped);
         // inquirer
         // .prompt([
         //     {
         //         name: "employee",
         //         type: "rawlist",
         //         message: "Which employee requires a role_id update?",
-        //         choices: result
+        //         choices: parsed.name
         //     }
         // ])
         // .then(function(answer) {
-        //     console.log(answer.employee)
+        //     console.log(result);
         // })
-    })}
-    
-    
-    // const sqlString = "UPDATE employees SET role_id="newrole" WHERE "+id;
-    // const query = connection.query(sqlString,
-    //     function(err, res) {
-    //         if (err) throw err;
-    //         console.log(res.affectedRows + " products updated!\n");
-    //         // Call deleteProduct AFTER the UPDATE completes
-    //         deleteProduct();
-    //     });
-    // }
-    
-    
-    
-    
-    
-    
+        
+        // const sqlString = "UPDATE employees SET role_id="newrole" WHERE "+id;
+        // const query = connection.query(sqlString,
+        //     function(err, res) {
+        //         if (err) throw err;
+        //         console.log(res.affectedRows + " products updated!\n");
+        //         // Call deleteProduct AFTER the UPDATE completes
+        //         deleteProduct();
+        //     });
+        // }
+    })
+}
+
+
+
+
+
+// ADD A DEPARTMENT DURING addTo()
+function addDepartment(){
+    inquirer
+    .prompt({
+        name: "department",
+        type: "input",
+        message: "Enter new department name"
+    })
+    .then(function(answer){
+        connection.query("INSERT INTO departments SET ?",
+        {
+            name: answer.department
+        },
+        function(err){if(err) throw err;})
+        console.log(answer.department+" successfully added to departments"); 
+    }
+    )
+}
+
+// ADD A ROLE DURING addTo()
+function addRole(){
+    inquirer
+    .prompt({
+        name: "title",
+        type: "input",
+        message: "Enter new title"
+    },
+    {
+        name: "salary",
+        type: "input",
+        message: "Enter the salary"
+    })
+    .then(function(answer){
+        connection.query("INSERT INTO roles SET ?",
+        {name: answer.department},
+        function(err){if(err) throw err;})
+        console.log(answer.department+" successfully added to departments");
+        start(); 
+    })
+}
+
+
+// ADD AN EMPLOYEE DURING addTo()
+function addEmployee(){
+    // GET ALL ROLES TO GET POSSIBLE IDS
+    const roles=[]
+    connection.query("SELECT * FROM roles", 
+    function(err,result) {
+        if (err) throw err;
+        roles=result;
+    })
+    inquirer
+    .prompt(
+        {
+            name: "first",
+            type: "input",
+            message: "Enter their first name"
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Enter their last name"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Enter new title",
+            choices: roles
+        }
+        
+        )
+        .then(function(answer){
+            connection.query("INSERT INTO employees SET ?",
+                {
+                    first: answer.first,
+                    last: answer.last,
+                    role 
+                },
+                function(err){if(err) throw err;
+                })
+                console.log(answer.department+" successfully added to departments");
+                start(); 
+            })
+        }
+        
+    }
     
